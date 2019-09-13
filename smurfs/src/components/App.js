6cd1301d3d0067d06e783a271a757aaeb9654f37
Kemberly Eliscar <kemberlyeliscar@Kemberlys-MacBook-Air-3.local>
2019-09-13 13:19:07 -0400

    im having a hard time downloading all the correct dependencies

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
+import SmurfList from './smurfList'
+import SmurfFormik from './smurfForm'
 
 class App extends Component {
   render() {
@@ -8,11 +10,17 @@ class App extends Component {
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfFormik/>
+        <SmurfList/>
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    title: state.title,
+    smurf : state.smurfs
+  }
+}
 
-export default App;
+export default connect(mapStateToProps,{})(App);
