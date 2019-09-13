a2669d652724a6f2adccbf16a6c740a07675e9d1
Kemberly Eliscar <kemberlyeliscar@Kemberlys-MacBook-Air-3.local>
2019-09-13 12:22:58 -0400

    set up actions

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,10 +5,10 @@ import App from "./components/App";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-//import {reducer } from "/.reducers";
+import { reducer } from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 ReactDOM.render(
   <Provider store={store}>
     <App />
